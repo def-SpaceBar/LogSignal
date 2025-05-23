f2fd2ee0f9c13b96a17caff098df4de32c30445e
spacebar <96818328+def-SpaceBar@users.noreply.github.com>
2025-05-23 18:00:14 +0300

    moved subscription_manager to a separated file
    as the code gets longer its harder to navigate so this is necessary at this point

---
+++ subscription_manager.py
@@ -0,0 +1,36 @@
+import win32evtlog as evt
+from typing import Dict, List
+import json
+from collections import defaultdict
+import threading
+import xmltodict
+
+
+class SubscriptionManager:
+    def __init__(self):
+        self.sub_map = defaultdict(dict)
+
+    def register_sub(self, subscription: Dict) -> None:
+        self.sub_map.update(subscription)
+
+    def get_sub_data(self, subscription):
+        return self.sub_map
+
+    @staticmethod
+    def on_event(action, context, event_handle):
+        print(context)
+        if action == evt.EvtSubscribeActionDeliver:
+            event = evt.EvtRender(event_handle, evt.EvtRenderEventXml)
+            event = xmltodict.parse(event, xml_attribs=False)
+        return 0
+
+    @staticmethod
+    def start_sub(channel: str, query: str, sub_map_as_context: object):
+        subscription = evt.EvtSubscribe(
+            channel,  # Channel path
+            evt.EvtSubscribeToFutureEvents,  # Flags
+            Query=query,  # XML query
+            Callback=SubscriptionManager.on_event,  # Callback function (async)
+            Context=sub_map_as_context  # Context to pass to callback
+        )
+        return subscription
