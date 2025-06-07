572558853936b7676e3bdad34b71b353d8704500
spacebar <96818328+def-SpaceBar@users.noreply.github.com>
2025-06-07 17:59:14 +0300

    created variables library to prevent key errors

---
+++ variables/event_keys.py
@@ -0,0 +1,25 @@
+##### WINDOWS EVENT JSON KEYS
+main_event_key: str = 'Event'
+system_dict_key: str = 'System'
+event_data_dict: str = 'EventData'
+data_key: str = 'Data'
+process_id: str = 'processid'
+process_id_selector: str = '@ProcessID'
+thread_id: str = 'threadid'
+thread_id_selector: str = '@ThreadID'
+user_sid: str = 'usersid'
+user_sid_selector: str = '@UserID'
+time_created: str = 'timecreated'
+time_created_selector: str = '@SystemTime'
+provider: str = 'provider'
+provider_guid: str = 'providerguid'
+at_sign_name_selector: str = '@Name'
+at_sign_guid_selector: str = '@Guid'
+hashtag_text_selector: str = '#text'
+
+###################################
+
+
+##### PARSING EVENT JSON KEYS
+errors_key: str = 'errors'
+custom_parsing_error: str = 'custom_parser_not_found'
---
+++ variables/rule_keys.py
@@ -0,0 +1,7 @@
+# USED WHEN EVER NEEDED TO ACCESS DICT KEYS OF RULES
+offense_source_key: str = 'offense_source'
+rule_id_key: str = 'rule_id'
+rule_name_key: str = 'rule_name'
+rule_description_key: str = 'rule_description'
+rule_xml_key: str = 'rule_xml'
+subscription_channel_key: str = 'subscription_channel'
